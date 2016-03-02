fc612a40ca51ea5e84b7820ea75355ac1732d7d4
John Dinsmore <johndinsmore@JDubs-IMac.local>
2016-03-01 22:47:19 -0500

    Design Refactor

--- application/views/contact.php
+++ application/views/contact.php
@@ -1,7 +1,7 @@
 
 	
     <div class="row">
-        <div class="col-md-12 ">
+        <div class="col-md-12 contactContainer">
             <div class="well well-sm contactContainer">
                 <form class="form-horizontal" method="post">
                     <fieldset>
