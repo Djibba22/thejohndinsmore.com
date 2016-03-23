1a86262b426d26e0acc60fdd2c0e1671eb43ee45
John Dinsmore <johndinsmore@JDubs-IMac.local>
2016-03-22 20:23:16 -0400

    background change

--- application/views/projects.php
+++ application/views/projects.php
@@ -4,7 +4,7 @@
 <section>
 	<div class="row">
 		<div class="col-12-lg text-center">
-		  <h1>Project Gallery</h1>
+		  <h1 class="proj-title">Project Gallery</h1>
 		</div>
 	</div>
   <div class="container gal-container">
