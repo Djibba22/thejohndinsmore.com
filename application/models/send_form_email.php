2f276f5e69bec315bb9ff17718405b273640417e
John Dinsmore <johndinsmore@JDubs-IMac.local>
2016-03-23 14:25:48 -0400

    Email form PHP in place

--- application/models/send_form_email.php
+++ application/models/send_form_email.php
@@ -1,45 +1,72 @@
-<?php 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
-if(isset($_POST['email'])){
-
-	$email_to = "johnwaynedinsmore@gmail.com";
-	$email_subject = "TheJohnDinsmore.com Response";
-
-
-	function died($error){
-		echo "I am sorry , there were error(s) found with the form you submitted.";
-		echo "These errors are as such:</br></br>";
-		echo $error."</br></br>";
-		echo "Please go back and fix these errors.</br></br>";
-
-		die();
-	}
-
-	if(!isset($_POST['first_name']) ||
-		!isset($_POST['last_name']) ||
-		!isset($_POST['email']) ||
-		!isset($_POST['telephone']) ||
-		!isset($_POST['comments']) ||{
-
-		died('I am sorry, it appears you have failed in the attempt to fill out a simple email form. Go ahead, try again');
-	}
-
-	$first_name = $_POST['first_name'];// required
-
-	$last_name = $_POST['last_name']; //required
-
-	$email_from = $_POST['email']; //required
-
-	$telephone = $_POST['telephone']; //required
-
-	$comments = $_POST['comments']; //required
-
-
-	error_message = "";
-	$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-	if(!preg_match($email_exp,$email_from)) {
+<?php
+ 
+if(isset($_POST['email'])) {
+ 
+     
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+ 
+    $email_to = "johnwaynedinsmore@gmail.com";
+ 
+    $email_subject = "Contact from thejohndinsmore.com";
+ 
+     
+ 
+     
+ 
+    function died($error) {
+ 
+        // your error code can go here
+ 
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+ 
+        echo "These errors appear below.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "Please go back and fix these errors.<br /><br />";
+ 
+        die();
+ 
+    }
+ 
+     
+ 
+    // validation expected data exists
+ 
+    if(!isset($_POST['first_name']) ||
+ 
+        !isset($_POST['last_name']) ||
+ 
+        !isset($_POST['email']) ||
+ 
+        !isset($_POST['telephone']) ||
+ 
+        !isset($_POST['comments'])) {
+ 
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+ 
+    }
+ 
+     
+ 
+    $first_name = $_POST['first_name']; // required
+ 
+    $last_name = $_POST['last_name']; // required
+ 
+    $email_from = $_POST['email']; // required
+ 
+    $telephone = $_POST['telephone']; // not required
+ 
+    $comments = $_POST['comments']; // required
+ 
+     
+ 
+    $error_message = "";
+ 
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
  
     $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
  
@@ -113,11 +140,15 @@ $headers = 'From: '.$email_from."\r\n".
  
  
  
-<!-- include your own success html here -->
+<html>
+  <body>
+    <h1>Thank you so much for reaching out. I willl</h1>
+  </body>
+</html>
  
  
  
-Thank you for reaching out to me. I will be in touch with you as soon as possible.
+
  
  
  
