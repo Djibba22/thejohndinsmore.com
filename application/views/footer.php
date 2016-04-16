e1aa25950546abc74d200f35e3d8c322d3c13e16
djibba22 <johnwaynedinsmore@gmail.com>
2016-04-16 13:32:53 -0400

    adjusted background

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,6 @@
  <footer class="footer-primary">
  	<ul class="social list-inline">
- 		<li class="icon-social-facebook"><a href="https://www.facebook.com/john.dinsmore.319">Facebook</a></li>
+ 		<li class="icon-social-facebook"><a href="https://www.facebook.com/john.dinsmore.319" target="_BLANK">Facebook</a></li>
  		<li class="icon-social-linkedin"><a href="/public/images/linkedincrack.png">LinkedIn</a></li>
  		<li class="icon-social-github"><a href="https://github.com/Djibba22/djibba22.github.io">LinkedIn</a></li>
  	</ul>
