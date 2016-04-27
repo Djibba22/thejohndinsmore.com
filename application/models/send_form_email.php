5373b124c8029305ce0d0a5369f9c3445ff289da
djibba22 <johnwaynedinsmore@gmail.com>
2016-04-27 19:15:09 -0400

    trouble shooting contact form

--- application/models/send_form_email.php
+++ application/models/send_form_email.php
@@ -1,162 +1,143 @@
 <?php
+if(mail("receipient@domain.com","A Subject Here","Hi there,\nThis email was sent using PHP's mail function."))
+print "Email successfully sent";
+else
+print "An error occured";
  
-if(isset($_POST['email'])) {
+// if(isset($_POST['email'])) {
  
-     
+//     $email_to = "johnwaynedinsmore@gmail.com";
  
-    // EDIT THE 2 LINES BELOW AS REQUIRED
+//     $email_subject = "Contact from thejohndinsmore.com";
  
-    $email_to = "johnwaynedinsmore@gmail.com";
+//     function died($error) {
  
-    $email_subject = "Contact from thejohndinsmore.com";
+//         // your error code can go here
  
-     
+//         echo "We are very sorry, but there were error(s) found with the form you submitted. ";
  
-     
+//         echo "These errors appear below.<br /><br />";
  
-    function died($error) {
+//         echo $error."<br /><br />";
  
-        // your error code can go here
+//         echo "Please go back and fix these errors.<br /><br />";
  
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+//         die();
  
-        echo "These errors appear below.<br /><br />";
+//     }
+//    // validation expected data exists
  
-        echo $error."<br /><br />";
+//     if(!isset($_POST['first_name']) ||
  
-        echo "Please go back and fix these errors.<br /><br />";
+//         !isset($_POST['last_name']) ||
  
-        die();
+//         !isset($_POST['email']) ||
  
-    }
+//         !isset($_POST['telephone']) ||
  
-     
+//         !isset($_POST['comments'])) {
  
-    // validation expected data exists
+//         died('We are sorry, but there appears to be a problem with the form you submitted.');       
  
-    if(!isset($_POST['first_name']) ||
- 
-        !isset($_POST['last_name']) ||
- 
-        !isset($_POST['email']) ||
- 
-        !isset($_POST['telephone']) ||
- 
-        !isset($_POST['comments'])) {
- 
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
- 
-    }
+//     }
  
      
  
-    $first_name = $_POST['first_name']; // required
+//     $first_name = $_POST['first_name']; // required
  
-    $last_name = $_POST['last_name']; // required
+//     $last_name = $_POST['last_name']; // required
  
-    $email_from = $_POST['email']; // required
+//     $email_from = $_POST['email']; // required
  
-    $telephone = $_POST['telephone']; // not required
+//     $telephone = $_POST['telephone']; // not required
  
-    $comments = $_POST['comments']; // required
- 
-     
- 
-    $error_message = "";
- 
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
- 
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
- 
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$first_name)) {
- 
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(!preg_match($string_exp,$last_name)) {
- 
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($comments) < 2) {
- 
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($error_message) > 0) {
- 
-    died($error_message);
- 
-  }
- 
-    $email_message = "Form details below.\n\n";
+//     $comments = $_POST['comments']; // required
  
      
  
-    function clean_string($string) {
+//     $error_message = "";
  
-      $bad = array("content-type","bcc:","to:","cc:","href");
+//     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
  
-      return str_replace($bad,"",$string);
+//   if(!preg_match($email_exp,$email_from)) {
  
-    }
+//     $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+ 
+//   }
+ 
+//     $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+//   if(!preg_match($string_exp,$first_name)) {
+ 
+//     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+ 
+//   }
+ 
+//   if(!preg_match($string_exp,$last_name)) {
+ 
+//     $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+ 
+//   }
+ 
+//   if(strlen($comments) < 2) {
+ 
+//     $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+ 
+//   }
+ 
+//   if(strlen($error_message) > 0) {
+ 
+//     died($error_message);
+ 
+//   }
+ 
+//     $email_message = "Form details below.\n\n";
  
      
  
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
+//     function clean_string($string) {
  
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+//       $bad = array("content-type","bcc:","to:","cc:","href");
  
-    $email_message .= "Email: ".clean_string($email_from)."\n";
+//       return str_replace($bad,"",$string);
  
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
- 
-    $email_message .= "Comments: ".clean_string($comments)."\n";
+//     }
  
      
  
-     
+//     $email_message .= "First Name: ".clean_string($first_name)."\n";
  
-// create email headers
+//     $email_message .= "Last Name: ".clean_string($last_name)."\n";
  
-$headers = 'From: '.$email_from."\r\n".
+//     $email_message .= "Email: ".clean_string($email_from)."\n";
  
-'Reply-To: '.$email_from."\r\n" .
+//     $email_message .= "Telephone: ".clean_string($telephone)."\n";
  
-'X-Mailer: PHP/' . phpversion();
+//     $email_message .= "Comments: ".clean_string($comments)."\n";    
  
-@mail($email_to, $email_subject, $email_message, $headers);  
+// // create email headers
+ 
+// $headers = 'From: '.$email_from."\r\n".
+ 
+// 'Reply-To: '.$email_from."\r\n" .
+ 
+// 'X-Mailer: PHP/' . phpversion();
+ 
+// @mail($email_to, $email_subject, $email_message, $headers);  
  
 ?>
- 
- 
- 
-<html>
+
+<!-- <html>
   <body>
     <div class="response">
     </br>
     <h2>Thank you so much for reaching out. I will get back to you as soon as possible</h2>
     </div>
   </body>
-</html>
+</html> -->
  
+// <?php
  
+// }
  
-
- 
- 
- 
-<?php
- 
-}
- 
-?>
+// ?>
