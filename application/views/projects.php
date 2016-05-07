fff89cf43c49a8518f0ea3e5b5be47e4cabda3fb
djibba22 <johnwaynedinsmore@gmail.com>
2016-05-07 08:51:25 -0400

    gallery Image fixes

--- application/views/projects.php
+++ application/views/projects.php
@@ -1,5 +1,5 @@
 
-	
+
 
 <section>
 	<div class="row">
@@ -13,14 +13,14 @@
     <div class="col-md-8 col-sm-12 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#1">
-          <img src="/public/images/Goodle.gif">
+          <img src="public/assets/images/Goodle.gif">
         </a>
         <div class="modal fade" id="1" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="/public/images/Goodle.gif">
+                <img src="public/assets/images/Goodle.gif">
               </div>
                 <div class="col-md-12 description">
                   <h4>Drawn in pencil, painted with photoshop, added layers for the animation </h4>
@@ -33,14 +33,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#2">
-          <img src="/public/images/Tabasco.jpg">
+          <img src="public/assets/images/Tabasco.jpg">
         </a>
         <div class="modal fade" id="2" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="/public/images/Tabasco.jpg">
+                <img src="public/assets/images/Tabasco.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>Complex gradient meshes and lighting then warped paper label for 3D glass appearence </h4>
@@ -53,14 +53,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#3">
-          <img src="/public/images/JD_GD.jpg">
+          <img src="public/assets/images/JD_GD.jpg">
         </a>
         <div class="modal fade" id="3" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="/public/images/JD_GD.jpg">
+                <img src="public/assets/images/JD_GD.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>Old business card layout with complete abstract effects</h4>
@@ -73,14 +73,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#4">
-          <img src="/public/images/Smart_Fin.jpg">
+          <img src="public/assets/images/Smart_Fin.jpg">
         </a>
         <div class="modal fade" id="4" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="/public/images/Smart_Fin.jpg">
+                <img src="public/assets/images/Smart_Fin.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>Technical drawing design, product prototyping, painted and shadows created in Photoshop</h4>
@@ -93,14 +93,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#5">
-          <img src="/public/images/TravelPoster.jpg">
+          <img src="public/assets/images/TravelPoster.jpg">
         </a>
         <div class="modal fade" id="5" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="/public/images/TravelPoster.jpg">
+                <img src="public/assets/images/TravelPoster.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>Shapes layered in Illustrator using advanced gradients</h4>
@@ -113,14 +113,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#6">
-          <img src="/public/images/Flyshirt4.jpg">
+          <img src="public/assets/images/Flyshirt4.jpg">
         </a>
         <div class="modal fade" id="6" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="/public/images/Flyshirt4.jpg">
+                <img src="public/assets/images/Flyshirt4.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>Clothing design sent with application to Fly Racing created in Illustrator</h4>
@@ -133,14 +133,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#7">
-          <img src="/public/images/Rhinorpion_3_Saturated2.jpg">
+          <img src="public/assets/images/Rhinorpion_3_Saturated2.jpg">
         </a>
         <div class="modal fade" id="7" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-          <img src="/public/images/Rhinorpion_3_Saturated2.jpg">
+          <img src="public/assets/images/Rhinorpion_3_Saturated2.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>Completeley hand drawn in pencil then shapes and shadow layers,  sponge brush painting technique in photoshop</h4>
@@ -153,14 +153,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#8">
-          <img src="/public/images/MoviePoster.jpg">
+          <img src="public/assets/images/MoviePoster.jpg">
         </a>
         <div class="modal fade" id="8" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-          <img src="/public/images/MoviePoster.jpg">
+          <img src="public/assets/images/MoviePoster.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>Freehand Illustrator, all line work and shapes created in one shot #RobotGuns</h4>
@@ -173,14 +173,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#9">
-           <img src="/public/images/walker.jpg">
+           <img src="public/assets/images/walker.jpg">
         </a>
         <div class="modal fade" id="9" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                 <img src="/public/images/walker.jpg">
+                 <img src="public/assets/images/walker.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4> Pencil Recreational Drawing</h4>
@@ -193,14 +193,14 @@
     <div class="col-md-8 col-sm-12 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#10">
-            <img src="/public/images/me_portrait.jpg">
+            <img src="public/assets/images/me_portrait.jpg">
         </a>
         <div class="modal fade" id="10" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="/public/images/me_portrait.jpg">
+                <img src="public/assets/images/me_portrait.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>Illustrator self portrait, lighting and perspective tracing higlights of a photo</h4>
@@ -224,14 +224,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#11">
-          <img src="/public/images/App_proto.jpg">
+          <img src="public/assets/images/App_proto.jpg">
         </a>
         <div class="modal fade" id="11" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="/public/images/App_proto.jpg">
+                <img src="public/assets/images/App_proto.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>Protoype Layout, rapid prototyping with Adobe Illustrator. I created the logo in Illustrator as well</h4>
@@ -244,14 +244,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#12">
-          <img src="/public/images/Wormburner2.png">
+          <img src="public/assets/images/Wormburner2.png">
         </a>
         <div class="modal fade" id="12" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="/public/images/Wormburner2.png">
+                <img src="public/assets/images/Wormburner2.png">
               </div>
                 <div class="col-md-12 description">
                   <h4>Angular, Firebase, Node.js application for Locating golf courses and Keeping Score.Check it out => <a class="gal-link" href="https://wormburnergolf.herokuapp.com/">WORMBURNER</a> </h4>
@@ -264,14 +264,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#13">
-            <img src="/public/images/WormBurnerPHP.png">
+            <img src="public/assets/images/WormBurnerPHP.png">
         </a>
         <div class="modal fade" id="13" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-               <img src="/public/images/WormBurnerPHP.png">
+               <img src="public/assets/images/WormBurnerPHP.png">
               </div>
                 <div class="col-md-12 description">
                   <h4>Built in PHP, this robust application pulls data from an API, utilizes the ion-auth libray for codeIgniter and sports CRUD operations with MYSQL. The code => <a class="gal-link" href="https://github.com/Djibba22/wormburner.git">PHP v1.0 WORMBURNER</a></h4>
@@ -284,14 +284,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#14">
-          <img src="/public/images/PortfolioSite1.png">
+          <img src="public/assets/images/PortfolioSite1.png">
         </a>
         <div class="modal fade" id="14" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-          <img src="/public/images/PortfolioSite1.png">
+          <img src="public/assets/images/PortfolioSite1.png">
               </div>
                 <div class="col-md-12 description">
                   <h4>This was an older version of my portfolio site using Jquery animations and Css3 transitions</h4>
@@ -304,14 +304,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#15">
-          <img src="/public/images/PennyPincher.png">
+          <img src="public/assets/images/PennyPincher.png">
         </a>
         <div class="modal fade" id="15" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-          <img src="/public/images/PennyPincher.png">
+          <img src="public/assets/images/PennyPincher.png">
               </div>
                 <div class="col-md-12 description">
                   <h4>This was my first real collaborative application built in Node using the Yelp API</h4>
@@ -343,5 +343,3 @@
     </div>
   </div>
 </section>
-
-
